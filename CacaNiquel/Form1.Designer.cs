9ba083476f9f10a7ad6528c7f3217c0b5f2bd01c
larissaCarvalho23 <carvalholb95@hotmail.com>
2020-09-01 21:22:24 -0300

    teste-commit

---
+++ CacaNiquel/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CacaNiquel
+{
+    partial class FrmCaca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCaca));
+            this.pcb1 = new System.Windows.Forms.PictureBox();
+            this.pcb2 = new System.Windows.Forms.PictureBox();
+            this.pcb3 = new System.Windows.Forms.PictureBox();
+            this.pcb4 = new System.Windows.Forms.PictureBox();
+            this.pcb5 = new System.Windows.Forms.PictureBox();
+            this.pcb6 = new System.Windows.Forms.PictureBox();
+            this.pcb7 = new System.Windows.Forms.PictureBox();
+            this.pcb8 = new System.Windows.Forms.PictureBox();
+            this.pcb9 = new System.Windows.Forms.PictureBox();
+            this.btnJogar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcb1
+            // 
+            this.pcb1.Image = global::CacaNiquel.Properties.Resources.frank;
+            this.pcb1.Location = new System.Drawing.Point(16, 12);
+            this.pcb1.Name = "pcb1";
+            this.pcb1.Size = new System.Drawing.Size(113, 85);
+            this.pcb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb1.TabIndex = 0;
+            this.pcb1.TabStop = false;
+            this.pcb1.Click += new System.EventHandler(this.pcb1_Click);
+            // 
+            // pcb2
+            // 
+            this.pcb2.Image = global::CacaNiquel.Properties.Resources.abobora__2_;
+            this.pcb2.Location = new System.Drawing.Point(187, 12);
+            this.pcb2.Name = "pcb2";
+            this.pcb2.Size = new System.Drawing.Size(117, 85);
+            this.pcb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb2.TabIndex = 1;
+            this.pcb2.TabStop = false;
+            // 
+            // pcb3
+            // 
+            this.pcb3.Image = global::CacaNiquel.Properties.Resources.diabo;
+            this.pcb3.Location = new System.Drawing.Point(374, 12);
+            this.pcb3.Name = "pcb3";
+            this.pcb3.Size = new System.Drawing.Size(114, 85);
+            this.pcb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb3.TabIndex = 2;
+            this.pcb3.TabStop = false;
+            // 
+            // pcb4
+            // 
+            this.pcb4.Image = global::CacaNiquel.Properties.Resources.diabo;
+            this.pcb4.Location = new System.Drawing.Point(16, 124);
+            this.pcb4.Name = "pcb4";
+            this.pcb4.Size = new System.Drawing.Size(113, 92);
+            this.pcb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb4.TabIndex = 3;
+            this.pcb4.TabStop = false;
+            // 
+            // pcb5
+            // 
+            this.pcb5.Image = global::CacaNiquel.Properties.Resources.fantasma;
+            this.pcb5.Location = new System.Drawing.Point(373, 124);
+            this.pcb5.Name = "pcb5";
+            this.pcb5.Size = new System.Drawing.Size(130, 92);
+            this.pcb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb5.TabIndex = 4;
+            this.pcb5.TabStop = false;
+            this.pcb5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pcb6
+            // 
+            this.pcb6.Image = global::CacaNiquel.Properties.Resources.bruxa;
+            this.pcb6.Location = new System.Drawing.Point(187, 124);
+            this.pcb6.Name = "pcb6";
+            this.pcb6.Size = new System.Drawing.Size(114, 92);
+            this.pcb6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb6.TabIndex = 5;
+            this.pcb6.TabStop = false;
+            // 
+            // pcb7
+            // 
+            this.pcb7.Image = global::CacaNiquel.Properties.Resources.bruxa;
+            this.pcb7.Location = new System.Drawing.Point(12, 249);
+            this.pcb7.Name = "pcb7";
+            this.pcb7.Size = new System.Drawing.Size(117, 96);
+            this.pcb7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb7.TabIndex = 6;
+            this.pcb7.TabStop = false;
+            // 
+            // pcb8
+            // 
+            this.pcb8.Image = global::CacaNiquel.Properties.Resources.caipira;
+            this.pcb8.Location = new System.Drawing.Point(174, 249);
+            this.pcb8.Name = "pcb8";
+            this.pcb8.Size = new System.Drawing.Size(130, 96);
+            this.pcb8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb8.TabIndex = 7;
+            this.pcb8.TabStop = false;
+            // 
+            // pcb9
+            // 
+            this.pcb9.Image = global::CacaNiquel.Properties.Resources.abobora__2_;
+            this.pcb9.Location = new System.Drawing.Point(373, 249);
+            this.pcb9.Name = "pcb9";
+            this.pcb9.Size = new System.Drawing.Size(115, 96);
+            this.pcb9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb9.TabIndex = 8;
+            this.pcb9.TabStop = false;
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.Location = new System.Drawing.Point(212, 382);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(106, 40);
+            this.btnJogar.TabIndex = 9;
+            this.btnJogar.Text = "JOGAR";
+            this.btnJogar.UseVisualStyleBackColor = true;
+            this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
+            // 
+            // FrmCaca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(553, 457);
+            this.Controls.Add(this.btnJogar);
+            this.Controls.Add(this.pcb9);
+            this.Controls.Add(this.pcb8);
+            this.Controls.Add(this.pcb7);
+            this.Controls.Add(this.pcb6);
+            this.Controls.Add(this.pcb5);
+            this.Controls.Add(this.pcb4);
+            this.Controls.Add(this.pcb3);
+            this.Controls.Add(this.pcb2);
+            this.Controls.Add(this.pcb1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Name = "FrmCaca";
+            this.Text = "CacaNiquel";
+            this.Load += new System.EventHandler(this.FrmCaca_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb9)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcb1;
+        private System.Windows.Forms.PictureBox pcb2;
+        private System.Windows.Forms.PictureBox pcb3;
+        private System.Windows.Forms.PictureBox pcb4;
+        private System.Windows.Forms.PictureBox pcb5;
+        private System.Windows.Forms.PictureBox pcb6;
+        private System.Windows.Forms.PictureBox pcb7;
+        private System.Windows.Forms.PictureBox pcb8;
+        private System.Windows.Forms.PictureBox pcb9;
+        private System.Windows.Forms.Button btnJogar;
+    }
+}
+
